c71c1aee12a53301ce661388d43ac8e01a4c9c6d
Heejong Ahn <heejongahn@gmail.com>
2016-10-31 15:15:14 +0900

    Replace build tool (stylus & babel-cli -> webpack)

---
+++ entry.js
@@ -0,0 +1,2 @@
+require('./src/bluemoon.styl');
+require('./src/bluemoon.js');
---
+++ webpack.config.babel.js
@@ -0,0 +1,38 @@
+import path from 'path';
+import webpack from 'webpack';
+
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import autoprefixer from 'autoprefixer';
+
+const plugins = [
+  new ExtractTextPlugin('bluemoon.css'),
+];
+
+const distPath = path.resolve('./dist');
+
+export default {
+  entry: './entry.js',
+  output: {
+    path: distPath,
+    publicPath: '/',
+    filename: 'bluemoon.js',
+  },
+  resolve: {
+    extensions: ['', '.js', '.styl']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        query: { presets: ['es2015'] }
+      },
+      {
+        test: /\.styl$/,
+        loader: ExtractTextPlugin.extract('style-loader', ['css!postcss!stylus'])
+      },
+    ]
+  },
+  plugins: plugins,
+  postcss: () => { [ autoprefixer ] }
+};
